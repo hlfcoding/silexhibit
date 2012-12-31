766f779b9c89cf993a9bbc0f66235cdda31222e8
Peng Wang <peng@pengxwang.com>
2012-12-30 20:03:14 -0800

    site: Misc cleanup.
    - SASS updated to 3.2.4.
    - Remove trailing whitespace.

--- site/sh/lib.sh
+++ site/sh/lib.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# Run from site/ root.
 
 cd js
+curl https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js -o "jquery.js"
 curl http://modernizr.com/downloads/modernizr.js -o "modernizr.js"
 curl http://underscorejs.org/underscore.js -o "underscore.js"
 curl https://raw.github.com/jrburke/requirejs/master/require.js -o "require.js"
+
+# TODO: scss includes
+
 cd ..
