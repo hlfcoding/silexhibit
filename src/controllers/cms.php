7073d631aea606d3fadfa9efcc7511eb2f558358
Peng Wang <peng@pengxwang.com>
2014-03-25 17:57:01 -0700

    Remove starter server scripts.

--- src/controllers/cms.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$cms = $app['controllers_factory'];
-$cms->get('/', function () use ($app) { return 'Home page'; });
-
-return $cms;
--- src/controllers/site.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$site = $app['controllers_factory'];
-$site->get('/', function () use ($app) {
-  return $app['templater']->render('Hello {{planet}}', array('planet' => 'World!'));
-});
-
-return $site;
