20593efc6c2c8e839a7a70798321a45e295797ee
Peng Wang <peng@pengxwang.com>
2017-12-24 18:05:52 -0800

    Rewrite 2: fix pengxwang tablet drawer regression.

--- theme/pengxwang/web/extend.js
+++ theme/pengxwang/web/extend.js
@@ -275,7 +275,7 @@
     let navElement = indexElement.querySelector('nav');
     let accordion = Accordion.extend(navElement);
 
-    if (layoutElement.offsetWidth < parseFloat(getComputedStyle(layoutElement).maxWidth)) {
+    if (document.body.clientWidth < parseFloat(getComputedStyle(layoutElement).maxWidth)) {
       if (getComputedStyle(navElement).display === 'none') {
         const expandedClass = 'js-expanded';
         const visibleClass = 'js-expanded-visible';
