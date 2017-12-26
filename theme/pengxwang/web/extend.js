f29b4b6984716ba33790ce78fb11b8929f4e1bfe
Peng Wang <peng@pengxwang.com>
2017-12-25 23:56:40 -0800

    Rewrite 2: minor pengxwang theme script refactor.

--- theme/pengxwang/web/extend.js
+++ theme/pengxwang/web/extend.js
@@ -268,14 +268,51 @@
     mixinNames: ['event'],
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
+  let setup = {};
+  setup.extension = {
+    accordion({ navElement }) {
+      Accordion.extend(navElement);
+    },
+    slideshow({ slideshowElement }) {
+      if (slideshowElement === null) { return; }
+      let slideshow = Slideshow.extend(slideshowElement);
+      let counterElement = slideshowElement.querySelector('.counter');
+      slideshowElement.addEventListener('hlfssslidechange', (event) => {
+        let { element, index } = event.detail;
+        counterElement.textContent = `${index + 1}`;
+      });
+    },
+    tips({ indexElement, navElement, footerElement, postElement, articleElement }) {
+      const { Tip } = HLF;
+      let navTip = Tip.extend(navElement.querySelectorAll('[title]'), {
+        snapTo: 'y', contextElement: navElement, viewportElement: indexElement
+      });
+      let footerTip = Tip.extend(footerElement.querySelectorAll('[title]'), {
+        snapTo: 'x', contextElement: footerElement, viewportElement: indexElement
+      });
+      Array.from(articleElement.children).forEach((sectionElement) => {
+        if (sectionElement.classList.contains('external')) {
+          let externalTip = Tip.extend(sectionElement.querySelectorAll('[title]'), {
+            snapTo: 'y', contextElement: sectionElement, viewportElement: postElement
+          });
+          return;
+        }
+        let tip = Tip.extend(sectionElement.querySelectorAll('[title]'), {
+          snapTo: 'x', contextElement: sectionElement, viewportElement: postElement
+        });
+      });
+    },
+  };
 
+  document.addEventListener('DOMContentLoaded', () => {
     let indexElement = document.querySelector('#index');
     let layoutElement = indexElement.parentElement;
-    let postElement = document.querySelector('#post');
-
     let navElement = indexElement.querySelector('nav');
-    let accordion = Accordion.extend(navElement);
+    let footerElement = indexElement.querySelector('footer');
+
+    let postElement = document.querySelector('#post');
+    let articleElement = postElement.querySelector('article');
+    let slideshowElement = postElement.querySelector('.slideshow');
 
     if (document.body.clientWidth < parseFloat(getComputedStyle(layoutElement).maxWidth)) {
       if (getComputedStyle(navElement).display === 'none') {
@@ -309,41 +346,14 @@
       }
     }
 
-    let articleElement = postElement.querySelector('article');
-    let slideshowElement = postElement.querySelector('.slideshow');
-    if (slideshowElement !== null) {
-      let slideshow = Slideshow.extend(slideshowElement);
-      let counterElement = slideshowElement.querySelector('.counter');
-      slideshowElement.addEventListener('hlfssslidechange', (event) => {
-        let { element, index } = event.detail;
-        counterElement.textContent = `${index + 1}`;
-      });
-    }
+    setup.extension.accordion({ navElement });
+    setup.extension.slideshow({ slideshowElement });
+    setup.extension.tips({ indexElement, navElement, footerElement, postElement, articleElement });
+
     let printElement = postElement.querySelector('button.print');
     if (printElement !== null) {
       printElement.addEventListener('click', () => print());
     }
-
-    const { Tip } = HLF;
-    let navTip = Tip.extend(navElement.querySelectorAll('[title]'), {
-      snapTo: 'y', contextElement: navElement, viewportElement: indexElement
-    });
-    let footerElement = document.querySelector('#index > footer');
-    let footerTip = Tip.extend(footerElement.querySelectorAll('[title]'), {
-      snapTo: 'x', contextElement: footerElement, viewportElement: indexElement
-    });
-    Array.from(articleElement.children).forEach((sectionElement) => {
-      if (sectionElement.classList.contains('external')) {
-        let externalTip = Tip.extend(sectionElement.querySelectorAll('[title]'), {
-          snapTo: 'y', contextElement: sectionElement, viewportElement: postElement
-        });
-        return;
-      }
-      let tip = Tip.extend(sectionElement.querySelectorAll('[title]'), {
-        snapTo: 'x', contextElement: sectionElement, viewportElement: postElement
-      });
-    });
-
   });
 
 }());
